f2eca560b3e5295d73dafc8e873cd1a364c2edd7
Daniel Mauderer <daniel090798@gmail.com>
2026-07-10 10:01:10 +0200

    add python to server

--- modules/nixos/server/containers.nix
+++ modules/nixos/server/containers.nix
@@ -1,4 +1,5 @@
-_: {
+{ pkgs, ... }:
+{
   # Turn the dev Podman (from modules/nixos/dev) into a service host: a
   # Docker-API socket at /run/docker.sock for compose/Ansible, timed prune.
   virtualisation.podman = {
@@ -7,4 +8,8 @@ _: {
   };
 
   virtualisation.oci-containers.backend = "podman";
+
+  # Ansible runs its modules on the managed node through a Python interpreter
+  # (it ships Python over SSH but needs one present to execute); provide it.
+  environment.systemPackages = [ pkgs.python3 ];
 }
